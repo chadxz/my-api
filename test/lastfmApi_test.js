15026ae31201f020cd2d0d330863ad27b683884e
Chad McElligott <chad.mcelligott@gmail.com>
2013-11-04 20:13:22 -0600

    adding some constructor tests

--- test/lastfmApi_test.js
+++ test/lastfmApi_test.js
@@ -3,11 +3,45 @@ module.exports = (function () {
   var lastfmApi = require('../lib/lastfmApi');
 
   return {
-    constructorReturnsInstanceOfLastfmUser: function constructorReturnsInstanceOfLastfmUser(test) {
+    constructorWorks: function (test) {
       test.expect(1);
 
       var lastfmUserApi = new lastfmApi.LastfmUser('', '');
       test.ok(lastfmUserApi instanceof lastfmApi.LastfmUser);
+
+      test.done();
+    },
+
+    constructorWorksWithoutNewStatement: function (test) {
+      test.expect(1);
+
+      var lastfmUserApi = lastfmApi.LastfmUser('', '');
+      test.ok(lastfmUserApi instanceof lastfmApi.LastfmUser);
+
+      test.done();
+    },
+
+    constructorSetsProperties: function (test) {
+      test.expect(3);
+
+      var apiKey = 'someapikey',
+          userName = 'someusername',
+          format = 'someformat',
+          lastfmUserApi = new lastfmApi.LastfmUser(apiKey, userName, format);
+
+      test.strictEqual(lastfmUserApi.apiKey, apiKey);
+      test.strictEqual(lastfmUserApi.user, userName);
+      test.strictEqual(lastfmUserApi.format, format);
+
+      test.done();
+    },
+
+    constructorSetsDefaultFormat: function (test) {
+      test.expect(1);
+
+      var lastfmUserApi = new lastfmApi.LastfmUser('', '');
+
+      test.ok(lastfmUserApi.format && lastfmUserApi.format !== '');
 
       test.done();
     }
