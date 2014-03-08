449532f0b9d79833dabc9bb4d010c53349de1e6c
Chad McElligott <cmcelligott@digium.com>
2014-03-08 17:43:12 -0600

    adding skip/limit functionality for paging on pinboard endpoint

--- routes/pinboard.js
+++ routes/pinboard.js
@@ -1,15 +1,34 @@
 module.exports = (function () {
 
   var redis = require('../lib/redis'),
-      pinboardUtils = require('../lib/pinboardUtils');
+      pinboardUtils = require('../lib/pinboardUtils'),
+      sharedUtils = require('../lib/sharedUtils');
 
   function showAllPosts(req, res) {
-    redis.get(pinboardUtils.redisKeys.posts, function (err, value) {
-      res.send(value);
+    redis.get(pinboardUtils.redisKeys.posts, function (err, jsonPosts) {
+      var pageParams;
+      var posts;
+
+      try {
+        posts = JSON.parse(jsonPosts);
+      } catch(e) {
+        res.send(500);
+        return;
+      }
+
+      try {
+        pageParams = sharedUtils.getPageParams(req);
+      } catch (e) {
+        res.send(400, { error: e.message });
+        return;
+      }
+
+      var results = sharedUtils.getPage(posts, pageParams.skip, pageParams.limit);
+      res.json(results);
     });
   }
 
   return {
     showAllPosts: showAllPosts
-  }
+  };
 })();
