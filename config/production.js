d23472b2482cb212356fe6e2326ab96fc4458055
Chad McElligott <chad.mcelligott@gmail.com>
2016-04-15 11:14:07 -0500

    Add placeholder production config

---
+++ config/production.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  // placeholder to suppress node-config warning about no config for environment
+  // please use the custom environment variables for actual production deploys!
+};
